251ca433ac6fb8138d1010fbbd4ebd255ec49017
John Paul <johnpaulnarvasa6@gmail.com>
2025-05-18 19:26:20 +0800

    added card and layout

---
+++ components/BaseCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-app>
+        <v-container>
+            <v-row>
+                <v-col cols="12" sm="6" md="4" lg="3" v-for="n in 20" :key="n">
+                    <v-card
+                        :title="'Card title ' + n"
+                        :subtitle="'Subtitle ' + n"
+                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris."
+                        variant="outlined"
+                        rounded="lg"
+                        color=""
+                        elevation="2"
+                    >
+                        <v-card-actions>
+                            <v-btn color="primary" variant="elevated">Button {{ n }}</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-app>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-app id="inspire">
+        <v-navigation-drawer v-model="drawer">
+            <!--  -->
+            <slot name="navigation" />
+        </v-navigation-drawer>
+
+        <v-app-bar>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+            <v-app-bar-title>Application</v-app-bar-title>
+        </v-app-bar>
+
+        <v-main>
+            <!--  -->
+            <slot name="default" />
+        </v-main>
+    </v-app>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const drawer = ref(null);
+</script>
+
+<script>
+export default {
+    data: () => ({ drawer: null }),
+};
+</script>
